f9156abc1f14c4c9ec2543317cdb17fc7d60ed0f
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2024-03-04 20:28:55 -0500

    changing check locations

--- code/gameselect/main.js
+++ code/gameselect/main.js
@@ -1,16 +1,4 @@
- // Function to check if the user is logged in
-    function checkLoggedIn() {
-        // Get the value of the loggedIn cookie
-        var loggedIn = document.cookie.replace(/(?:(?:^|.*;\s*)loggedIn\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 
-        // If the loggedIn cookie is not set to "true", redirect to webgfa.com
-        if (loggedIn !== "true") {
-            window.location.href = "https://webgfa.com";
-        }
-    }
-
-    // Call the function to check if the user is logged in when the page loads
-    checkLoggedIn();
 document.addEventListener('keydown', function(event) {
     if (event.ctrlKey && event.shiftKey && event.key === 'C') {
         const cookies = document.cookie.split(';');
@@ -19,11 +7,7 @@ document.addEventListener('keydown', function(event) {
     }
 });
  // Function to retrieve the value of a cookie by its name
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  };
+
     // Function to delete a cookie by name
     function deleteCookie(name) {
         document.cookie = name + '=; Max-Age=-99999999;';
@@ -31,7 +15,7 @@ document.addEventListener('keydown', function(event) {
 
     // Event listener for keydown event
     document.addEventListener('keydown', function(event) {
-        // Check if Ctrl+Shift+Q is pressed
+        // Check if Ctrl+Shift+L is pressed
         if (event.ctrlKey && event.shiftKey && event.key === 'L') {
             // Delete cookies
             deleteCookie('loggedIn');
---
+++ code/loginchecks/rblxcheck.js
@@ -0,0 +1,20 @@
+// Function to check if the user is logged in
+function checkLoggedIn() {
+    // Get the value of the loggedIn cookie
+    const loggedIn = getCookie("loggedInRblx");
+
+    // If the loggedIn cookie is not set to "true", redirect to webgfa.com
+    if (loggedIn !== "true") {
+        window.location.href = "https://webgfa.com/games/rblx";
+    }
+}
+
+// Function to retrieve the value of a cookie by its name
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+// Call the function to check if the user is logged in when the page loads
+checkLoggedIn();
