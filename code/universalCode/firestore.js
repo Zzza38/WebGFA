ef062b924b71f5d37e7982ad258928864430b0e0
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2024-04-03 17:43:58 -0400

    fixed admin cookie

--- code/universalCode/firestore.js
+++ code/universalCode/firestore.js
@@ -1,10 +1,3 @@
-// Function to get cookie
-function getCookie(name) {
-	const value = `; ${document.cookie}`;
-	const parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop().split(';').shift();
-}
-window.getCookie = getCookie;
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
@@ -81,7 +74,13 @@ console.error('Error getting field from Firestore:', error);
 return null;
 }
 }
-
+// Function to get cookie
+function getCookie(name) {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
+}
+window.getCookie = getCookie;
 
 
 window.app = app;
