1b3f972ce94fb3177d85740de0192110314d4f97
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2024-04-19 18:52:59 -0400

    removed analytics on front page

--- code/index/main.js
+++ code/index/main.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
 
         // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 // Reference the form and the alertText
 const form = document.getElementById('loginForm');
