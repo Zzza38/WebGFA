9e6fa04b58e023daec1b3146b176fb6bd98bf05a
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2024-04-22 10:11:17 -0400

    debugging firestore.js, added tetris

--- gameselect/main.js
+++ gameselect/main.js
@@ -25,3 +25,27 @@ document.addEventListener('keydown', function(event) {
             window.location.href = "https://webgfa.com";
         }
     });
+    import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; // Import Firestore modules
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+    
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyAMOJV2z02dLtMb8X1uWDGkDx6ysrzBcUo",
+        authDomain: "webgfa-games.firebaseapp.com",
+        projectId: "webgfa-games",
+        storageBucket: "webgfa-games.appspot.com",
+        messagingSenderId: "553239008504",
+        appId: "1:553239008504:web:b91fba77cf0f131849170d",
+        measurementId: "G-5W79NYJZ11"
+    };
+    
+            // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    //const analytics = getAnalytics(app);
+    const firestore = getFirestore(app);
+
+    const docRef = doc(firestore, 'data', 'ccfs');
+    const docData = await getDoc(docRef);
+    console.log(docData);
+    window.docData = docData;
