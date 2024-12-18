9931eebe28f53385b5b283bd8def46411c8de756
Zion Aronov <zwatch3@gmail.com>
2024-12-18 17:48:56 -0500

    - normalize path didnt work correctly, infinite loop of 301

--- src/server.js
+++ src/server.js
@@ -84,9 +84,11 @@ function normalizePath(reqPath) {
     const trimmedPath = reqPath.replace(/\/+$/, ''); // Remove trailing slashes
     const pathSegments = trimmedPath.split('/'); // Split the path into segments
     let fileName = '/' + pathSegments.pop().replace(/\/+$/, ''); // Get the last segment (file name)
-    
+
     if (fileName == '/') {
         fileName = '/index.html'
+    } else if (reqPath.endsWith('/')) {
+        fileName += '/'
     }
 
     return pathSegments.join('/') + fileName;
