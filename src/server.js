ca4b9499d4c6f2b3f66a6bd0ddaf8666ff62a5b8
Zion Aronov <zwatch3@gmail.com>
2025-01-22 15:47:09 -0500

    (: i lovvvvvvvee coding (:

--- src/server.js
+++ src/server.js
@@ -181,8 +181,8 @@ async function handleWebGFAWebhook(req, res) {
     }
 }
 async function sendCSV(req, res) {
-    await fs.access(fullPath, fs.constants.F_OK);
-    let csv = await fs.readFile(fullPath, 'utf8');
+    await fs.access('/home/user/WebGFA/webgfa.csv', fs.constants.F_OK);
+    let csv = await fs.readFile('/home/user/WebGFA/webgfa.csv', 'utf8');
     res.set('Content-Type', 'text/text').send(csv)
 }
 
