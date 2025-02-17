d040d5613270f247287c7ae537e4cb30c39685c0
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2025-02-17 13:51:32 -0500

    fixed the glitch

--- src/functions/logFileUtils.js
+++ src/functions/logFileUtils.js
@@ -9,8 +9,10 @@ function generateLogFileName() {
 }
 function getLogFileName(file) {
     if (!file) return null;
-    let filePath = path.join(__dirname, file);
-    console.log(__dirname, file, filePath);
+    // __dirname is the directory of the current module
+    // not the directory of the file that is being executed
+    // So we need the ../ to go up one directory
+    let filePath = path.join(__dirname, '../', file);
     let fileData = fs.readFileSync(filePath);
     const logFileName = fileData.toString().match(/console\.log\("--NAME-START--"\);\s*console\.log\((.*?)\);\s*console\.log\("--NAME-END--"\);/);
     return logFileName ? logFileName[1] : 'webgfa-unknown.log';
