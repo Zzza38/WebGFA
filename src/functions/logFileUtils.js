bf929d84afbf3d0119409b5142c2662102a9ecfc
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2025-02-17 14:32:41 -0500

    fixed that too

--- src/functions/logFileUtils.js
+++ src/functions/logFileUtils.js
@@ -18,8 +18,11 @@ function getLogFileName(file) {
     return logFileName ? logFileName[1] : 'webgfa-unknown.log';
 }
 function copyLogFile(oldLog, newLog) {
-    const logPath = path.join(__dirname, oldLog);
-    const newLogPath = path.join(__dirname, newLog);
+    // __dirname is the directory of the current module
+    // not the directory of the file that is being executed
+    // So we need the ../ to go up one directory
+    const logPath = path.join(__dirname, '../', oldLog);
+    const newLogPath = path.join(__dirname, '../', newLog);
     fs.copyFileSync(logPath, newLogPath);
 }
 module.exports = { 
