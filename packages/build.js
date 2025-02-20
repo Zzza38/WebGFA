0ef9462757b9653abf4eb94043228ef13b1a9b54
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2025-02-20 14:45:23 -0500

    test cuz interstellar was cloning at the package.json, but __dirname was package, its being weird

--- packages/build.js
+++ packages/build.js
@@ -5,7 +5,10 @@ function pipe(proc) {
     proc.stderr.pipe(process.stderr);
 }
 
+pipe(exec('pwd'))
+process.chdir(__dirname);
+pipe(exec('pwd'))
 // Interstellar
-pipe(exec('git clone --branch Ad-Free https://github.com/UseInterstellar/Interstellar'));
-pipe(exec('npm install', { cwd: './Interstellar' }));
+//pipe(exec('git clone --branch Ad-Free https://github.com/UseInterstellar/Interstellar'));
+//pipe(exec('npm install', { cwd: './Interstellar' }));
 
