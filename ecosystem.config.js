dc5a69d0b2d94f16d3f47e92fb0eecd7921b2f0e
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2025-03-26 13:52:10 -0400

    Update ecosystem.config.js
    fixing broken ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const config = require('./config.json');
 const fs = require('fs');
-fs.existsSync(path.join(__dirname, 'logs')) || fs.mkdirSync(path.join(__dirname, 'logs'), { recursive: true });
+fs.existsSync(path.join(__dirname, 'logs')) || fs.mkdirSync(path.join(__dirname, 'logs'), {
+    recursive: true
+});
 
 function insertChdir(filePath) {
     if (!fs.existsSync(filePath)) {
@@ -12,7 +14,7 @@ function insertChdir(filePath) {
     let content = fs.readFileSync(filePath, 'utf8');
 
     // Check if it already has `process.chdir`
-    if (content.includes("process.chdir(__dirname);") || content.includes("process.chdir(dirname);" || content.includes("process.chdir(__currentDirectoryPath);")) {
+    if (content.includes("process.chdir(__dirname);") || content.includes("process.chdir(dirname);" || content.includes("process.chdir(__currentDirectoryPath);"))) {
         console.log("process.chdir is already present.");
         return;
     }
@@ -28,13 +30,10 @@ function insertChdir(filePath) {
     }
 
     // Prepend the new code
-    content = insertStatement + content;
-    fs.writeFileSync(filePath, content, 'utf8');
-    console.log("Inserted process.chdir handling at the beginning of", filePath);
+    content = insertStatement + content; fs.writeFileSync(filePath, content, 'utf8'); console.log("Inserted process.chdir handling at the beginning of", filePath);
 }
 
-const apps = [
-  {
+const apps = [{
     name: "webgfa",
     script: "./src/server.js",
     instances: 2,
@@ -46,50 +45,51 @@ const apps = [
     autorestart: true,
     watch: false,
     ignore_watch: [
-      "node_modules",
-      "static",
-      "logs"
+        "node_modules",
+        "static",
+        "logs"
     ],
     max_memory_restart: "1G",
     env: {
-      NODE_ENV: "production"
+        NODE_ENV: "production"
     }
-  }
-];
+}];
 if (config.installed.interstellar) {
-  insertChdir("./packages/Interstellar/index.js");
-  apps.push({
-    name: "interstellar",
-    script: "./packages/Interstellar/index.js",
-    instances: 1,
-    out_file: path.join(__dirname, 'logs/interstellar.log'),
-    error_file: path.join(__dirname, 'logs/interstellar.log'),
-    merge_logs: true,
-    log_date_format: "YYYY-MM-DD HH:mm Z",
-    autorestart: true,
-    watch: false,
-    env: {
-      NODE_ENV: "production",
-      "PORT": config.ports.interstellar
-    }
-  });
+    insertChdir("./packages/Interstellar/index.js");
+    apps.push({
+        name: "interstellar",
+        script: "./packages/Interstellar/index.js",
+        instances: 1,
+        out_file: path.join(__dirname, 'logs/interstellar.log'),
+        error_file: path.join(__dirname, 'logs/interstellar.log'),
+        merge_logs: true,
+        log_date_format: "YYYY-MM-DD HH:mm Z",
+        autorestart: true,
+        watch: false,
+        env: {
+            NODE_ENV: "production",
+            "PORT": config.ports.interstellar
+        }
+    });
 }
 if (config.installed.webssh) {
-  apps.push({
-    name: "webssh",
-    script: "./packages/webssh2/app/index.js",
-    instances: 1,
-    out_file: path.join(__dirname, 'logs/webssh.log'),
-    error_file: path.join(__dirname, 'logs/webssh.log'),
-    merge_logs: true,
-    log_date_format: "YYYY-MM-DD HH:mm Z",
-    autorestart: true,
-    watch: false,
-    env: {
-      NODE_ENV: "production",
-      "PORT": config.ports.webssh
-    }
-  });
+    apps.push({
+        name: "webssh",
+        script: "./packages/webssh2/app/index.js",
+        instances: 1,
+        out_file: path.join(__dirname, 'logs/webssh.log'),
+        error_file: path.join(__dirname, 'logs/webssh.log'),
+        merge_logs: true,
+        log_date_format: "YYYY-MM-DD HH:mm Z",
+        autorestart: true,
+        watch: false,
+        env: {
+            NODE_ENV: "production",
+            "PORT": config.ports.webssh
+        }
+    });
 }
 
-module.exports = { apps };
+module.exports = {
+    apps
+};
